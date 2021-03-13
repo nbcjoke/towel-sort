db39afad40b4eaa21ceb3fe891c028a6a57dbbf8
nbcjoke <79941934+nbcjoke@users.noreply.github.com>
2021-03-13 20:49:58 +0300

    towel-sort function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+matrix.reduce(function(prev, item, index) {
+if(index % 2 === 0) {
+item = item.sort((a, b) => a > b ? 1: -1);
+} else {
+item = item.sort((a, b) => a > b ? -1: 1);
+}
+return prev.concat(...item);
+}, [])
 }
